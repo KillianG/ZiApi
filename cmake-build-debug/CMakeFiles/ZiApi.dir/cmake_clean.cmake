7112b20efb6c7ab0599ca95f3f937933f4c8757a
killian <killian.gardahaut@epitech.eu>
2019-01-22 13:06:28 +0100

    Init repo

---
+++ cmake-build-debug/CMakeFiles/ZiApi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZiApi.dir/main.cpp.o"
+  "ZiApi.pdb"
+  "ZiApi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZiApi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
